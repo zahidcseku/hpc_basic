d7c8fd4c1a7d366542a862aa26f38c29b93175ba
Md Zahidul Islam <zahidcseku@gmail.com>
2024-12-21 07:21:04 +1000

    added arguments

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Set variables
-LOCAL_PATH="C:/zahids files/git_repos/Leveraging HPC Clusters for Big Data ML Training"
 BRANCH="main"
 MAIN_SCRIPT="main.py"
+START_ID=0
+END_ID=100
+
+# Local variables
+LOCAL_PATH="C:/zahids files/git_repos/Leveraging HPC Clusters for Big Data ML Training"
 PYTHON_PATH="python"
 
 # Change to the repository directory
@@ -26,7 +29,7 @@ if [ $? -eq 0 ]; then
     echo "Repository synced successfully"
     
     # Run the Python script
-    $PYTHON_PATH $MAIN_SCRIPT
+    $PYTHON_PATH $MAIN_SCRIPT $LOCAL_PATH $START_ID $END_ID
 else
     echo "Failed to sync repository"
     exit 1
